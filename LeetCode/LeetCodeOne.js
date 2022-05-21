1560a92322f764e91d281b44db63e1d4c5ecba5e
Diego (D) <actual.mensch0r@icloud.com>
2022-05-21 13:51:02 -0500

    Find pivot index

--- LeetCode/LeetCodeOne.js
+++ LeetCode/LeetCodeOne.js
@@ -8,3 +8,25 @@ var runningSum = function (nums) {
   let lastSum = 0;
   return nums.map((x) => (lastSum += x));
 };
+
+// Find pivot index
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  //loop array
+  //slice left and reduce
+  //slice right and reduce
+  //check if both are equal return index
+  //if not return -1
+  for (let i = 0; i < nums.length; i++) {
+    let left = nums.slice(0, i).reduce((a, c) => a + c, 0);
+    let right = nums.slice(i + 1, nums.length).reduce((a, c) => a + c, 0);
+    if (left === right) {
+      return i;
+    }
+  }
+  return -1;
+};
