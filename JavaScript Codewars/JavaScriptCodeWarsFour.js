42762b15ad169bf8163eb4e253081bb67b713f2c
Diego (D) <actual.mensch0r@icloud.com>
2023-02-21 05:59:35 -0600

    Multiple of index 8 kyu javascript

--- JavaScript Codewars/JavaScriptCodeWarsFour.js
+++ JavaScript Codewars/JavaScriptCodeWarsFour.js
@@ -4,3 +4,19 @@ var a = 'dev';
 var b = 'Lab';
 
 var name = a + b;
+
+// Multiple of index (8 Kyu)
+
+function multipleOfIndex(array) {
+  let holdArray = [];
+
+  // loop over the array
+  // check if the num is a multiple
+  // push it to a new array if it is
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % i === 0) {
+      holdArray.push(array[i]);
+    }
+  }
+  return holdArray;
+}
