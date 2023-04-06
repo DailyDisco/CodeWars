8a88dfd4e75443b7ba7b406f1669eef655cc6183
Diego (D) <actual.mensch0r@icloud.com>
2023-04-06 14:23:48 -0500

    How many lightsabers do you own? 8 Kyu Typescript

---
+++ Typescript Codewars/TypescriptCodeWarsOne.ts
@@ -0,0 +1,7 @@
+// How many lightsabers do you own? 8 Kyu Typescript
+export function howManyLightsabersDoYouOwn(name?: any): number {
+  // So, how many?
+  return name == 'Zach' ? 18 : 0;
+}
+
+// The question mark (?) after name attribute marks it as optional.
