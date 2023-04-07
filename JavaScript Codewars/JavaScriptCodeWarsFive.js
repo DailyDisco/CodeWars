6c40f852dd5041828aacd8229e94b40a5f24ac12
Diego (D) <actual.mensch0r@icloud.com>
2023-04-07 13:46:25 -0500

    Training JS #10: loop statement --for 8 Kyu

--- JavaScript Codewars/JavaScriptCodeWarsFive.js
+++ JavaScript Codewars/JavaScriptCodeWarsFive.js
@@ -149,3 +149,19 @@ function isVow(a) {
     /[aeiou]/.test(String.fromCharCode(x)) ? String.fromCharCode(x) : x
   );
 }
+
+// Training JS #10: loop statement --for 8 Kyu
+function pickIt(arr) {
+  var odd = [],
+    even = [];
+  //coding here
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+
+  return [odd, even];
+}
