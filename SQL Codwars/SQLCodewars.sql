2d1b242f9b510b7c2ff09b6d4b3c30e1985559fe
Diego (D) <actual.mensch0r@icloud.com>
2023-04-07 15:33:44 -0500

    SQL Grasshopper: Select Columns 8 Kyu SQL

--- SQL Codwars/SQLCodewars.sql
+++ SQL Codwars/SQLCodewars.sql
@@ -4,3 +4,8 @@ SELECT id, name, age
 FROM people
 WHERE age > 50 
 ORDER BY age DESC
+
+-- SQL Grasshopper: Select Columns 8 Kyu SQL
+-- Type your SQL here
+SELECT custid, custname, custstate
+FROM customers
