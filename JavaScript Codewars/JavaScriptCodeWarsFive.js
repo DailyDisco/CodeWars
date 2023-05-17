770092248f0c368967a38241b2935031c29d20fc
Diego (D) <actual.mensch0r@icloud.com>
2023-05-16 22:12:36 -0500

    // UEFA EURO 2016 8 Kyu Javascript

--- JavaScript Codewars/JavaScriptCodeWarsFive.js
+++ JavaScript Codewars/JavaScriptCodeWarsFive.js
@@ -250,3 +250,20 @@ function switcheroo(x) {
   }
   return newArr.join('');
 }
+// UEFA EURO 2016 8 Kyu Javascript
+function uefaEuro2016(teams, scores) {
+  if (scores[0] > scores[1]) {
+    return (
+      'At match ' + teams[0] + ' - ' + teams[1] + ', ' + teams[0] + ' won!'
+    );
+  } else if (scores[1] > scores[0]) {
+    return (
+      'At match ' + teams[0] + ' - ' + teams[1] + ', ' + teams[1] + ' won!'
+    );
+  } else {
+    return (
+      'At match ' + teams[0] + ' - ' + teams[1] + ', ' + 'teams played draw.'
+    );
+  }
+  // your code...
+}
