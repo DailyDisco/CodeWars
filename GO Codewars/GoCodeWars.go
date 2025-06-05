635df7b935f41618ec286ace167e77ddbd814407
Diego Espino <actual.mensch0r@icloud.com>
2025-06-05 18:15:22 -0500

    Is he gonna survive 8 Kyu Golang

--- GO Codewars/GoCodeWars.go
+++ GO Codewars/GoCodeWars.go
@@ -10,3 +10,20 @@ func SimpleMultiplication(n int) int {
   
   return n * 9 // your code here
 }
+
+// Is he gonna survive? (8 Kyu)
+
+func Hero(bullets, dragons int) bool {
+  // your code
+  
+  // P = int bullets, int dragons
+  // R = true if enough bullets, false if not enough bullets
+  // E = 10 Bullets, 5 dragons = true, 9 bullets, 5 dragons = false
+  // P = multiply number of dragons by 2 and check if it's greater than the bullets
+  
+  if (bullets >= dragons * 2 ) {
+    return true
+  } else {
+    return false
+  }
+}
