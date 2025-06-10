8f8f48366b293087067733f5ffa07f2c3d893a39
Diego Espino <actual.mensch0r@icloud.com>
2025-06-10 12:10:39 -0500

    Learning TypeScript. Basic Types

--- Typescript Codewars/TypescriptCodeWarsOne.ts
+++ Typescript Codewars/TypescriptCodeWarsOne.ts
@@ -102,3 +102,44 @@ export function sameCase(a: string, b: string): number {
 
   return bothUpper || bothLower ? 1 : 0;
 }
+
+// Learning TypeScript. Basic Types 8 Kyu Typescript
+export var var1Boolean: boolean = true;
+export var var2Decimal: number = 13;
+export var var3Hex: number = 0xf00d;
+export var var4Binary: number = 0b111111;
+export var var5Octal: number = 0o744;
+
+export var var6String: string = 'Hello, world!';
+
+export var var7Array: (number | string | object)[] = [
+  1,
+  'test',
+  { a: 3 },
+  4,
+  5,
+];
+export var var8NumericArray: Array<number> = [1, 2, 3, 4, 5];
+
+export var var9Tuple: [string, number] = ['key', 12345];
+
+export enum Color {
+  Red = 1,
+  Green = 2,
+  Blue = 4,
+}
+
+export var var10Enum: Color = Color.Blue;
+
+export var var11ArrayOfAny: Array<any> = [1, 'test', { a: 3 }, 4, 5];
+
+export function var12VoidFunction(): void {
+  console.log('no return');
+}
+
+export var var13Null: null = null;
+export var var14Undefined: undefined = undefined;
+
+export function var15NeverFunction(): never {
+  throw new Error('This will never return');
+}
